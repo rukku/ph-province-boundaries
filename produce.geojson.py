4847f12f8f3e7a0818e8c47c7ee4e75daa890a77
RK Aranas <rk.aranas@gmail.com>
2013-11-17 02:59:55 +0800

    Add topojson and use ISO 3166 codes

--- produce.geojson.py
+++ produce.geojson.py
@@ -1,7 +1,13 @@
 #import subprocess
 from subprocess import call
 import shapefile
+import os
 from dbfpy import dbf 
+import csv
+
+#replace spaces
+def replacespaces(s):
+	return s.replace(' ', '\\ ')
 
 #list provinces
 def list_provinces():
@@ -63,29 +69,158 @@ def listrecords(shp):
 	for record in records:
 		print record[5]
 
-def listfiles():
-	pass
+#def movedirectories():	
+def create_subset(whole, datasource, level):
+	for subset in whole:
+		#command = 'ogr2ogr -f GeoJSON -where "%s IN (\'%s\')" %s.geojson %s' % (level, subset, subset, datasource)
+		#subprocess.call(command)
 
-def movedirectories():	
-	pass
+		outfile = "%s.geojson" % subset
+		
+		arguments = "%s IN (\'%s\')" % (level, subset)
+
+		call(['ogr2ogr','-f', 'GeoJSON','-where', arguments, outfile, datasource])
+
 #convert to topojson
 #pass
 
+def loadISOcodes():
+	with open('ISO.csv','rb') as f:
+		codes = dict()
+		reader = csv.reader(f)
+		for row in reader:
+			#print row[2], row[0]
+			codes[row[0]] = row[1].upper()
+			print 
+		return codes  
+
+def listFiles():
+    ascList = []
+    for r,d,f in os.walk("."):
+        for files in f:
+            if files.endswith(".geojson"):
+                #print os.path.join(r,files) 
+                out = os.path.join(r,files) + "\n" 
+                ascList.append(os.path.join(r,files))
+    return ascList
+
+def filenames(filepath):
+	return filepath[2:-8]
+
+def fixfilesnames(filepath):
+	names = []
+	for filepath in filepaths:
+		append(filenames(filepath))
+	return names
 
 #move to its own folder
+def json2folder(files, isocodes):
+	
+	count = 0
+	nomatch = open('nomatches.txt', 'wt')
+	isonames = open('isonames.csv','wt')
+	nm = []
+	shellscript = open('mass-topojson.sh', 'wt')
+	for f in files:
+		found = 0
+		name = filenames(f) 
+		for k,v in isocodes.items():
+			if name == v:
+			#	print k,v
+				filename = name + ".topojson"
+				jsonfile = name + ".geojson"
+				#print filename,type(filename), jsonfile, type(jsonfile)
+				
+				#command = replacespaces(command)
+				filename = replacespaces(filename)
+				jsonfile = replacespaces(jsonfile)
+				command = "topojson -o {0} {1}\n".format(filename, jsonfile)
+				shellscript.write(command)
+				isonames.write(name + "," + k + "\n")
+				print command
+				found = 1
+				#print filename
+				#call("topojson", "-o", filename, jsonfile, shell=False) #convert to topoJSON
+				#call() #create folder and move topoJSON and geoJSON to folder
+			else:
+				#
+				#print name
+				continue
+		if found == 0: 
+			nm.append(name)
 
+	newlist = list(set(nm))
+	print len(newlist)
+	for n in newlist:
+		print n
+		nomatch.write(n+"\n")			
+		#for k,v in isocodes.iteritems():
+			#print k,v
 
+def movefolders():
+	isonames = open('isonames.csv', 'rb')
+	ison = dict()
+	reader = csv.reader(isonames)
+	for row in reader:
+		ison[row[0]] = row[1]
+		#print row
+		#ison.append(row)
+	return ison
 #tests
 def est_list():
 	prov_list = list_provinces()
 	print prov_list
 
+def test_listFiles():
+	flist = listFiles()
+	for f in flist:
+		print filename(f)
+
+
+
+#prov = list_provinces()
+#create_subset(prov, 'citymuni.shp', 'PROVINCE')
+
+#create_subset(prov, 'citymuni.shp', 'PROVINCE')
+
+#test_listFiles()
+#filelist = listFiles()
+#isocodes = loadISOcodes()
+#son2folder(filelist, isocodes)
+
+movesh = open('topojsonmove.sh', 'wt')
+isonames = movefolders()
+for name, iso in isonames.iteritems():
+	name = replacespaces(name)
+	#print name
+	geoj = name +".geojson"
+	topoj = name +".topojson"
+
+	mkdirtext = "mkdir %s\n" % (iso)  
+	movegeojson = "mv %s ./%s/%s\n" % (geoj, iso, geoj)
+	movetopojson = "mv %s ./%s/%s\n" % (topoj, iso, topoj)
+
+	print mkdirtext
+	print movegeojson
+	print movetopojson
+	
+	movesh.write(mkdirtext)
+	movesh.write(movegeojson)
+	movesh.write(movetopojson)
+	#movetopojson = "mv %s %s" % () 
+#for k,v in isocodes.iteritems():
+#	print k,v
+
+
 #checkfields('citymuni.shp')
 #listrecords('citymuni.shp')
 #shp_to_geojson('citymuni.shp')
 #filename = 
+#fileList = listFiles()
+#for fiLe in fileList:
+#	print fiLe
 
-prov = list_provinces()
-create_subset(prov, 'citymuni.shp', 'PROVINCE')
+#prov = list_provinces()
+#create_subset(prov, 'citymuni.shp', 'PROVINCE')
 #test_list()
 #print subprocess.call('ogrinfo')
